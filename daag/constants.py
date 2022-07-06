636df03d5386b89607de937c24724dea1e4b1f33
hark130 <hark130@yahoo.com>
2022-07-06 17:03:50 -0500

    DAAG-1: constants initial commit
    Defined namedtuple Node
    Defined some package constants
    
    Changes to be committed:
    	new file:   daag/constants.py

---
+++ daag/constants.py
@@ -0,0 +1,16 @@
+"""Defines constants and data types for DAAG."""
+
+from collections import namedtuple
+from typing import Final
+
+
+# Title: uniquely identifying name (e.g., DAAG-1)
+# Component: major project component/epic (e.g., config, binary, test_class)
+# Type: type of ticket (e.g., test, production, research)
+# Blocked: tickets that block this node, AKA "blocked by" (e.g., DAAG-1, [DAAG-1, DAAG-2])
+# Blocks: tickets blocked by this node (e.g., DAAG-4, [DAAG-5, DAAG-6])
+# Status: current ticket status (e.g., Open, IP, Done)
+Node = namedtuple('Node', ['title', 'component', 'type', 'blocked', 'blocks', 'status'])
+
+OPEN_LIST_DELIM: Final[str] = '['  # Database entry delimiter indicating a list beginning
+CLOSE_LIST_DELIM: Final[str] = ']'  # Database entry delimiter indicating a list ending
